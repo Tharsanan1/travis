7ca9a6702d55e91ad4d7af78e835b46182a3447e
tkur2121 <tharsanan.kurukulasingam@syscolabs.com>
2018-09-17 17:51:59 +0530

    doc

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello Worldees!'))
+app.get('/', (req, res) => {
+    res.send('Hello Worldees!')
+    server.close();
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let server  = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
