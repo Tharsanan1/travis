13b2248be5d951988b535bd255a324f5a8aa5188
tkur2121 <tharsanan.kurukulasingam@syscolabs.com>
2018-09-19 10:37:26 +0530

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8000
 app.get('/', (req, res) => {
     res.send('Hello Worldees!')
     server.close();
-    // to close the server after a test browse.
+    // close the server after a test browse.
 })
 
 let server  = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
